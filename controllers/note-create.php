6abb03820cfc1d77e90c3cfa1b798027731b60ca
Alexandre <alexandreriosme@gmail.com>
2023-06-15 17:30:23 -0300

    S3E26: Always Escape Untrusted Input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$config = require 'config.php';
+$db = new Database($config['database'], 'root', 'root');
+
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  dd($_POST);
+  $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => 1
+  ]);
 }
 
 require('views/note-create.php');
