a74029bbb29d23b1fc1a2dde08bd3c78ce523976
xanderios <53008881+xanderios@users.noreply.github.com>
2024-03-04 14:13:13 -0300

    S4E33: Build a Better Router

--- Core/router.php
+++ Core/router.php
@@ -1,27 +1,61 @@
 <?php
 
-use Core\Response;
+namespace Core;
 
-function routeToController($uri, $routes)
-{
-  if (array_key_exists($uri, $routes)) {
-    require base_path($routes[$uri]);
-  } else {
-    abort();
+class Router {
+  protected $routes = [];
+
+  public function add($method, $uri, $controller)
+  {
+    $this->routes[] = [
+      'uri' => $uri,
+      'controller' => $controller,
+      'method' => $method
+    ];
+  }
+
+  public function get($uri, $controller)
+  {
+    $this->add('GET', $uri, $controller);
+  }
+
+  public function post()
+  {
+    $this->add('POST', $uri, $controller);
+  }
+
+  public function delete()
+  {
+    $this->add('DELETE', $uri, $controller);
+  }
+  
+  public function patch()
+  {
+    $this->add('PATCH', $uri, $controller);
+  }
+
+  public function put()
+  {
+    $this->add('PUT', $uri, $controller);
+  }
+
+  public function route($uri, $method)
+  {
+    foreach ($this->routes as $route) {
+      if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+        return require base_path($route['controller']);
+      }
+    }
+
+    $this->abort();
+  }
+
+  protected function abort($code = 404)
+  {
+    http_response_code($code);
+    
+    require base_path("views/{$code}.php");
+
+    die();
   }
 }
-
-
-function abort($code = Response::NOT_FOUND)
-{
-  http_response_code($code);
-
-  require base_path("views/{$code}.php");
-
-  die();
-}
-
-$routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-routeToController($uri, $routes);
--- views/notes/show.php
+++ views/notes/show.php
@@ -10,7 +10,8 @@ require base_path("views/partials/banner.php");
       <a href="/notes" class="text-blue-500 hover:underline">Go back</a>
     </p>
     <p><?= htmlspecialchars($note['body']) ?></p>
-    <form class="mt-6" method="post">
+    <form class="mt-6" method="POST">
+      <input type="hidden" name="_method" value="DELETE">
       <input type="hidden" name="id" value="<?= $note['id'] ?>">
       <button class="text-sm text-red-500">Delete</button>
     </form>
